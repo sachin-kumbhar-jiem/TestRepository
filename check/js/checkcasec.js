7fe9af2a951b826c63bfd1bdea2710ac18c0ed04
mayur.gaikwad@jiem.in <mayur.gaikwad@jiem.in>
2016-04-11 06:39:28 +0000

    Committed for ticket no 1488

--- check/js/checkcasec.js
+++ check/js/checkcasec.js
@@ -15,6 +15,92 @@ function CasecResult(){
 	};
 }
 
+function CaseckBrowserResult(){
+	this.result = false;
+	this.resultbrowser = function(){
+		if(this.result){
+			return "▲";
+		}
+	}
+}
+
+function CaseckOsResult(){
+	this.result = false;
+	this.resultos = function(){
+		if(this.result){
+			return "▲";
+		}
+	}
+}
+
+function CasecOsCheck(){
+	this.resultList = new Array();
+	this.resultList["os"] = new CaseckOsResult();
+	this.osInfo = new OsInfo();
+	this.resultList["os"].name = this.osInfo.getOsName();
+	this.resultList["os"].result = false;	
+		if ((this.osInfo.baseOs=="win" &&this.osInfo.version == 6.2)){
+				  this.resultList["os"].result = true;
+		}
+	this.totalResult = true;
+	for(var r in this.resultList){
+		if(!this.resultList[r].result){
+			this.totalResult = false;
+		}
+	}
+}
+
+
+function CasecBrowserCheck(){
+	
+	var CASEC_OS_LIST = ['win_Vista','win_7','win_8','win_81','win_NT10'];
+	this.resultList = new Array();
+	this.resultList["os"] = new CaseckBrowserResult();
+	this.resultList["browser"] = new CaseckBrowserResult();
+	this.osInfo = new OsInfo();
+	this.resultList["os"].name = this.osInfo.getOsName();
+	this.resultList["os"].result = false;
+	for(var i = 0; i < CASEC_OS_LIST.length; i++){
+		if(CASEC_OS_LIST[i] === this.osInfo.os){
+			this.resultList["os"].result = true;
+		}
+	}
+	
+	this.browserInfo = new BrowserInfo();
+	this.resultList["browser"].name = this.browserInfo.getBrowserName();
+	this.resultList["browser"].result = false;
+	if(this.resultList["os"].result && this.osInfo.baseOs=="win"){
+		if ((this.osInfo.version == 10 && 
+				  this.browserInfo.baseBrowser == "ie" && this.browserInfo.version == 9)) {
+				  this.resultList["browser"].result = true;
+		}
+		else if ((this.osInfo.version == 6.3 && 
+				  this.browserInfo.baseBrowser == "ie" && this.browserInfo.version == 9)) {
+				  this.resultList["browser"].result = true;
+		}
+		else if ((this.osInfo.version == 6.1 && 
+			      this.browserInfo.baseBrowser == "ie" && this.browserInfo.version == 9)) {
+				  this.resultList["browser"].result = true;
+		}
+		else if ((this.browserInfo.baseBrowser == "ie" && this.browserInfo.version == 7)){
+			    this.resultList["browser"].result = true;
+		}
+		
+		else if ((this.browserInfo.baseBrowser == "ie" && this.browserInfo.version == 8)){
+			    this.resultList["browser"].result = true;
+		}
+		
+		else if ((this.browserInfo.baseBrowser == "ie" && this.browserInfo.version == 10)){
+			    this.resultList["browser"].result = true;
+		}
+	}
+	this.totalResult = true;
+	for(var r in this.resultList){
+		if(!this.resultList[r].result){
+			this.totalResult = false;
+		}
+	}
+}
 
 function CasecCheck(){
 
@@ -44,7 +130,7 @@ function CasecCheck(){
 	this.resultList["browser"].name = this.browserInfo.getBrowserName();
 	this.resultList["browser"].result = false;
 	if(this.resultList["os"].result && this.osInfo.baseOs=="win"){
-		if ((this.browserInfo.baseBrowser == "microsoftedge") || 
+		if ((this.browserInfo.baseBrowser == "microsoftedge") ||
 			(this.browserInfo.baseBrowser == "ie" && this.browserInfo.version >= 6) ||
 			(this.browserInfo.baseBrowser == "chrome"/* && this.browserInfo.version >= 16*/)) {
 				this.resultList["browser"].result = true;
