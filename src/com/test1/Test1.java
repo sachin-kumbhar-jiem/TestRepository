38e935339e2662fc3b9bcde19e5c2ae9b0fb6306
JIEM-L40\Shailendra <shailendra.namdeo@jiem.in>
2018-10-10 12:56:47 +0530

    ssss

--- src/com/test1/Test1.java
+++ src/com/test1/Test1.java
@@ -2,7 +2,10 @@ package com.test1;
 
 public class Test1 {
 	public static void main(String[] args) {
+		System.out.println("Test Branch 2");
 		
+		
+
 	}
-  
+
 }
--- src/com/test1/Test2.java
+++ src/com/test1/Test2.java
@@ -3,6 +3,8 @@ package com.test1;
 public class Test2{
 	public static void main(String[] args) {
 		 System.out.println("Test Message ... ");
+		 
+		 
 	}
  
 }
